37c4eafc435816a28c30ec38b1d8ddf01df2db96
hannalahti <hannalahti19@gmail.com>
2025-02-20 14:55:43 -0500

    added basic file chooser

--- src/controllers/uploadInvoiceScreenController.java
+++ src/controllers/uploadInvoiceScreenController.java
@@ -7,8 +7,10 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -31,6 +33,23 @@ public class uploadInvoiceScreenController {
 
     @FXML
     void uploadNewInvoice(ActionEvent event) throws IOException, InterruptedException {
+
+        // initialize file chooser
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File");
+        // freeze current window, show filechooser, get selected file
+        File file = fileChooser.showOpenDialog(((Node) event.getSource()).getScene().getWindow());
+
+        if (file == null) { // no file selected
+            System.out.println("No file selected.");
+            return;
+        }
+
+        System.out.println("File selected: " + file);
+
+        // send file to server
+
+        // load verification screen
         Parent root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/fxml/verificationScreen.fxml"))));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
